7fa1d1f4c05da404dc908ceaaf2228021a4337ea
Peter_Kenna <treep78@gmail.com>
2016-10-13 22:35:04 -0400

    added card generating function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,18 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
-
 //Card Values
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-var firstCard = "King";
-var secondCard = "King";
+var numOfCards = 4;
+var cardList = [];
+var gameBoard = document.getElementById("game-board");
 
-if(firstCard == secondCard)
+function setupCards(nOC)
+{
+	for(var i = 0;i<nOC; i++)
+	{
+		var newDiv = document.createElement('div');
+		newDiv.className="card";
+		cardList.push(newDiv);
+		document.getElementById("game-board").appendChild(newDiv);
+	}
+}
+
+setupCards(numOfCards);
+/*if(firstCard == secondCard)
 {
 	alert("You found a match!");
 }
 else
 {
 	alert("Sorry, try again.");
-}
+}*/
